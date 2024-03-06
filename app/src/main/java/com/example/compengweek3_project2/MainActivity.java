9ef35c50bbac34c19822dbaf4f9cfee4bddf50d5
ogrenci <ogrenci@mac-iMac-4.local>
2024-03-06 15:39:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/compengweek3_project2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.compengweek3_project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity { //adaptor set eden metodu istediğim için listac kullandım
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Toast.makeText(this, (String)l.getItemAtPosition(position), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.countries)));
+    }
+}
